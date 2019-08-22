e4acd452a3029bb875770935e8959a07d4a28df7
julian viezens <jv@demodern.de>
2019-08-22 17:26:11 +0200

    woops imported the wrong project...QUICKFIX!

---
+++ Assets/Foot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Foot : MonoBehaviour
+{
+    public Transform anchorPoint;
+
+    public float legLength = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(anchorPoint != null)
+            legLength = (anchorPoint.position - transform.position).magnitude;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/LegController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegController : MonoBehaviour
+{
+    ConfigurableJoint joint;
+    public Vector3 euler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint = GetComponent<ConfigurableJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        joint.targetRotation = Quaternion.Euler(euler);
+    }
+}
