86ff88573c8351fdf9185970878814842ef450ae
Julian <viezensj@googlemail.com>
2019-08-28 00:36:54 +0200

    Added KenneyNL Isometric Prototype assets and made movement + dodge
    - dodge isn't invulnerable yet

---
+++ Assets/IsometricCharacterAnimationController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricCharacterAnimationController : MonoBehaviour
+{
+
+    Vector3 lastFacing = new Vector3(1, 1, 0);
+    bool isMoving = false;
+    float movingThreshhold = 0.01f;
+    Animator animator;
+
+    int facingXHash = Animator.StringToHash("FacingX");
+    int facingYHash = Animator.StringToHash("FacingY");
+    int isMovingHash = Animator.StringToHash("IsMoving");
+    int pickupHash = Animator.StringToHash("Pickup");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    public void Animate(Vector2 moveInput)
+    {
+        isMoving = moveInput.sqrMagnitude > movingThreshhold;
+
+        if (isMoving)
+        {
+            lastFacing = moveInput;
+        }
+
+        animator.SetFloat(facingXHash, lastFacing.x);
+        animator.SetFloat(facingYHash, lastFacing.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            //animator.SetTrigger(pickupHash);
+            animator.speed = 0;
+        }
+        else
+        {
+            animator.speed = 1;
+        }
+        animator.SetBool(isMovingHash, isMoving);
+    }
+}
---
+++ Assets/IsometricMovementController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricMovementController : MonoBehaviour
+{
+    public float moveSpeed = 4f;
+    public float dashDistance = 1f;
+    public float dashTime = 0.2f;
+
+    Vector2 moveInput;
+    float dash = 0;
+    Rigidbody2D rigid;
+    IsometricCharacterAnimationController animator;
+
+    bool isDashing = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<IsometricCharacterAnimationController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        if(!isDashing && Input.GetButtonDown("Jump"))
+        {
+            StartCoroutine(StartDash());
+        }
+        else if(!isDashing)
+        {
+            Vector2 adjustedMove = moveInput;
+            adjustedMove.y *= 0.5f;
+            rigid.MovePosition(rigid.position + adjustedMove.normalized * (moveSpeed * Time.deltaTime));
+        }
+
+        animator.Animate(moveInput);
+    }
+
+    IEnumerator StartDash()
+    {
+        isDashing = true;
+        dash = 0;
+        float t = 0;
+        Vector2 preDashPosition = rigid.position;
+        Vector2 dashDirection = moveInput.normalized;
+        do
+        {
+            rigid.MovePosition(preDashPosition + dashDirection * Mathf.Lerp(0, dashDistance, t));
+            t += Time.deltaTime / dashTime;
+            yield return null;
+        } while (t < 1.0f);
+        dash = 0;
+        isDashing = false;
+    }
+}
