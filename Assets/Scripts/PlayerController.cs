1e3b18b5e4bd9bc5844519bcab4a0be66cff90b7
Julian <viezensj@googlemail.com>
2019-08-30 00:16:30 +0200

    Added Rocketglove to PlayerController
    - kinda bugggy
    - movement doesnt feel good
    - not tested with controllers

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,22 @@ public class PlayerController : MonoBehaviour
 {
     public float playerSpeed = 1;
     public float jumpForce = 10;
+
+    public float rocketFistRecoil = 4;
     Vector2 moveInput = Vector2.zero;
 
     Rigidbody2D rigid;
+    Vector3 mouseDirection;
+
+    public Transform aimArm;
+
+    public RocketController rocketFist;
 
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        rocketFist.OnBreak += ReactivateAimArm;
     }
 
     // Update is called once per frame
@@ -28,5 +36,52 @@ public class PlayerController : MonoBehaviour
         {
             rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
+    }
+
+    private void Update()
+    {
+        Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseDirection = worldMousePos - transform.position;
+        //Aim at mouse
+        if (mouseDirection.sqrMagnitude > 0.01f)
+        {
+            // Get Angle in Radians
+            float AngleRad = Mathf.Atan2(mouseDirection.y, mouseDirection.x);
+            // Get Angle in Degrees
+            float AngleDeg = (180 / Mathf.PI) * AngleRad - 90;
+            // Rotate Object
+            aimArm.rotation = Quaternion.Euler(0, 0, AngleDeg);
+        }
+
+        if(Input.GetMouseButtonDown(0) && aimArm.gameObject.activeInHierarchy)
+        {
+            if(rocketFist.flightTime <= 0 ||!rocketFist.gameObject.activeInHierarchy)
+            {
+                aimArm.gameObject.SetActive(false);
+                rocketFist.gameObject.SetActive(false);
+                rocketFist.transform.rotation = aimArm.transform.rotation;
+                rocketFist.transform.position = aimArm.transform.position + aimArm.transform.up * 0.5f;
+                rocketFist.gameObject.SetActive(true);
+                rocketFist.Fire();
+                rigid.AddForce(-rocketFist.transform.up * rocketFistRecoil, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private void ReactivateAimArm()
+    {
+        StartCoroutine(DelayedActivate());
+    }
+
+    IEnumerator DelayedActivate()
+    {
+        yield return new WaitForSeconds(2.0f);
+        ActivateAimArm();
+    }
+
+    private void ActivateAimArm()
+    {
+        aimArm.gameObject.SetActive(true);
+        rocketFist.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -10,13 +10,34 @@ public class RocketController : MonoBehaviour
 
     Vector3 mouseDirection;
 
+    public float flightTime = 0; //How many seconds can the glove fly
+    float maxFlightTime = 3;
+
     public bool keyboardController = false;
     Vector2 moveInput = Vector2.zero;
+
+    public LayerMask breakMask;
+
+    SpriteRenderer spriteR;
+
+    public System.Action OnBreak;
 
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteR = GetComponent<SpriteRenderer>();
+    }
+
+    public void Fire()
+    {
+        flightTime = maxFlightTime;
+    }
+
+    private void LateUpdate()
+    {
+        spriteR.color = Color.Lerp(Color.green, Color.red, 1.0f - (flightTime / maxFlightTime));
+        if (flightTime <= 0) spriteR.color = Color.black;
     }
 
     // Update is called once per frame
@@ -44,19 +65,32 @@ public class RocketController : MonoBehaviour
             Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mouseDirection = worldMousePos - transform.position;
             bool leftMouse = Input.GetMouseButton(0);
+            if(flightTime > 0 && Input.GetMouseButtonDown(1))
+            {
+                Break();
+            }
             if (leftMouse)
             {
-                rigid.gravityScale = 0;
-                rigid.AddForce(mouseDirection * RocketAcceleration, ForceMode2D.Force);
-                rigid.velocity = Vector3.ClampMagnitude(rigid.velocity, maxVelocity);
-                if (rigid.velocity.sqrMagnitude > 0.01f)
+                if(flightTime > 0)
                 {
-                    // Get Angle in Radians
-                    float AngleRad = Mathf.Atan2(rigid.velocity.y, rigid.velocity.x);
-                    // Get Angle in Degrees
-                    float AngleDeg = (180 / Mathf.PI) * AngleRad - 90;
-                    // Rotate Object
-                    rigid.MoveRotation(AngleDeg);
+                    rigid.gravityScale = 0;
+                    rigid.AddForce(mouseDirection * RocketAcceleration, ForceMode2D.Force);
+                    flightTime -= Time.deltaTime;
+                    rigid.velocity = Vector3.ClampMagnitude(rigid.velocity, maxVelocity);
+                    if (rigid.velocity.sqrMagnitude > 0.01f)
+                    {
+                        // Get Angle in Radians
+                        float AngleRad = Mathf.Atan2(rigid.velocity.y, rigid.velocity.x);
+                        // Get Angle in Degrees
+                        float AngleDeg = (180 / Mathf.PI) * AngleRad - 90;
+                        // Rotate Object
+                        rigid.MoveRotation(AngleDeg);
+                    }
+
+                }
+                else
+                {
+                    Break();
                 }
             }
             else
@@ -65,5 +99,21 @@ public class RocketController : MonoBehaviour
             }
 
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if( breakMask == (breakMask | (1 << col.gameObject.layer)))
+        {
+            Break();
+            Debug.Log("OnCollisionEnter2D");
+        }
+    }
+
+    void Break()
+    {
+        flightTime = 0;
+        rigid.gravityScale = 1;
+        OnBreak?.Invoke();
     }
 }
